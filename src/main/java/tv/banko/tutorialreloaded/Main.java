0db25f73d388eb446c397a8b787017406563ba31
DerBanko <accounts@banko.tv>
2021-02-13 16:02:10 +0100

    Initial Commit

---
+++ src/main/java/tv/banko/tutorialreloaded/Main.java
@@ -0,0 +1,17 @@
+package tv.banko.tutorialreloaded;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
