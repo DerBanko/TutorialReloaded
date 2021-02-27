3797f7a4ad0ccde9d1dd1f7a2dd8f0cb8af42ce0
DerBanko <accounts@banko.tv>
2021-02-27 15:14:47 +0100

    Episode 2 of the coding series on youtube

--- src/main/java/tv/banko/tutorialreloaded/Main.java
+++ src/main/java/tv/banko/tutorialreloaded/Main.java
@@ -1,13 +1,21 @@
 package tv.banko.tutorialreloaded;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import tv.banko.tutorialreloaded.listeners.JoinListener;
+import tv.banko.tutorialreloaded.listeners.QuitListener;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_RED + "HALLO!");
 
+        PluginManager manager = Bukkit.getPluginManager();
+        manager.registerEvents(new JoinListener(), this);
+        manager.registerEvents(new QuitListener(), this);
     }
 
     @Override
---
+++ src/main/java/tv/banko/tutorialreloaded/listeners/JoinListener.java
@@ -0,0 +1,19 @@
+package tv.banko.tutorialreloaded.listeners;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class JoinListener implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        event.setJoinMessage(ChatColor.GREEN.toString() + ChatColor.UNDERLINE + player.getName() + " hat den Server betreten.");
+
+        player.sendMessage(ChatColor.GOLD + "Willkommen auf dem Server! \n\"Viel Spaß und viel Vergnügen (^:");
+    }
+}
